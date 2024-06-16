8eec171eee9a4a85cc8658ac54ac89a0932e8c76
gastawny <gastawny@gmail.com>
2024-06-16 13:44:51 -0300

    fix phases

--- src/app/(base)/login/action.ts
+++ src/app/(base)/login/action.ts
@@ -7,6 +7,9 @@ export async function submit(form: FormData) {
   console.log(form.get('group'), form.get('student'))
   const cookiesStore = cookies()
 
+  cookiesStore.delete('phase-1')
+  cookiesStore.delete('phase-2')
+
   cookiesStore.set('group', `${form.get('group')}`)
   cookiesStore.set('student', `${form.get('student')}`)
 
