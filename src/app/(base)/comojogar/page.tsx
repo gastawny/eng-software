e4216f8dd5ab8a2cf41e05d7152f51f4b491bdbe
gastawny <gastawny@gmail.com>
2024-06-16 21:09:22 -0300

    fix

--- src/app/(base)/comojogar/page.tsx
+++ src/app/(base)/comojogar/page.tsx
@@ -2,7 +2,7 @@ export default function ComoJogarPage() {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <iframe
-        className="w-[720px] h-[420px] 2xl:w-[1280px] 2xl:h-[720px] rounded-lg"
+        className="w-[720px] h-[420px] 2xl:w-[1220px] 2xl:h-[640px] rounded-lg"
         src="https://www.youtube.com/embed/Kf2Tk8FRL2M"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
       ></iframe>
